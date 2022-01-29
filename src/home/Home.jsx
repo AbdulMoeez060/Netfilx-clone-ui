963fbcdd6fee48e2b383817093d8b6f8791fb9b1
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-01-29 22:57:14 +0500

    added the featured page

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from '../components/featured/Featured';
 import Navbar from '../components/navbar/Navbar';
 import "./home.scss"
 
@@ -5,9 +6,8 @@ const Home = () => {
   return (
     <div className='home'>
         <Navbar/>
-        <img width="100%" src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+        <Featured type="movie"/>
 
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda saepe, eligendi blanditiis dolorem autem dolor quam voluptates nostrum doloribus, minima rem iure enim beatae laboriosam necessitatibus illo vero accusantium? Accusantium pariatur dolor harum quia soluta distinctio iusto obcaecati odit autem alias atque, rerum ut possimus unde est, quo doloribus libero officia iure repellendus at! Natus amet in distinctio ad quidem. Pariatur nemo, magnam qui ad consectetur soluta nostrum saepe. Doloremque corporis itaque magnam illum iure in cupiditate facere doloribus iste non eaque, provident dolores rem exercitationem reprehenderit sapiente suscipit recusandae officiis quibusdam qui quasi maxime, ea error culpa. Incidunt deserunt obcaecati nemo reiciendis alias aspernatur, quos a nulla repudiandae ipsum fuga facilis enim excepturi, in doloremque sapiente dolorem nesciunt veritatis nostrum dolore. Sed labore, corrupti, alias aperiam nemo, ad iusto asperiores blanditiis corporis natus placeat doloremque reiciendis! Temporibus quis animi ut asperiores ea eius ratione a, repellendus quidem deserunt molestiae nostrum, eligendi debitis dolorem culpa fugiat tenetur omnis. Ea aut reiciendis sit at consequuntur facere ipsam ut nemo doloribus. Commodi corporis voluptatem minus labore eius optio temporibus saepe ab sed natus, culpa ducimus iure cum nam dignissimos? Nemo, beatae porro sapiente ipsam neque animi delectus sint quaerat. Vitae labore voluptatibus ipsa, voluptas laboriosam natus mollitia magni eos iusto in voluptatum maxime aut fugiat eaque quam? Doloremque exercitationem recusandae unde et commodi labore at nisi, inventore iure sed minus deserunt aliquam non in ad, praesentium assumenda veniam. Illo eligendi magnam tenetur voluptates saepe tempora possimus? Dolores tempora optio sunt ratione. Omnis, eius doloremque! Ipsam, ex? Quae nemo facere laboriosam molestiae commodi in ab cum, recusandae soluta sapiente totam. Voluptatum omnis dolorum quo! Dolorem, consectetur laboriosam excepturi harum velit hic exercitationem omnis dignissimos in quidem non ipsam expedita aperiam totam tempore quam tenetur repudi
     </div>
   );
 };

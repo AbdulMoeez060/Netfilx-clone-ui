2086e1d15979a5163dfbe4af4afc38c34fdb3ac2
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-01-30 22:58:43 +0500

    added hover to list items

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,3 +1,4 @@
+import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from "@material-ui/icons";
 import "./listItem.scss";
 
 export default function ListItem() {
@@ -6,7 +7,12 @@ export default function ListItem() {
         <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" alt="" />
 
         <div className="itemInfo">
-            
+            <div className="icons">
+                <PlayArrow/>
+                <Add/>
+                <ThumbUpAltOutlined/>
+                <ThumbDownAltOutlined/>
+            </div>
         </div>
     </div>
   );

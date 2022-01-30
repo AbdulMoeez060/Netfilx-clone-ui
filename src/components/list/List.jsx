b63edfce86acac36b5497131e3e6d9029d6cf4da
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-01-30 22:12:19 +0500

    added new component list item and made the demi list

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,9 +1,29 @@
+import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@material-ui/icons";
+import ListItem from "../listItem/ListItem";
 import "./list.scss";
 
 export default function List() {
   return (
     <div className='list'>
+      <span className="listTitle">Continue To Watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined/>
+        
+        <div className="container">
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
 
+        </div>
+
+
+        <ArrowForwardIosOutlined/>
+      </div>
     </div>
     );
 }

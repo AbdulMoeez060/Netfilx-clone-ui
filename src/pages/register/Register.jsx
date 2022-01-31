9b4a189e0c777a78caa58a12c63ff7cef5bfba78
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-01-31 23:59:53 +0500

    completed the register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -4,13 +4,20 @@ import "./register.scss";
 export default function Register() {
 
     const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
 
     const emailRef  = useRef();
+    const passwordRef = useRef();
 
     const handleStart = ()=>{
-        
+        setEmail(emailRef.current.value);
     }
 
+    const handleFinish = ()=>{
+        setPassword(passwordRef.current.value);
+
+    }
   return (
     <div className="register">
 
@@ -30,12 +37,20 @@ export default function Register() {
             <p>
             Ready to watch? Enter your email to create or restart your membership.
             </p>
+            {!email ? (
+                    <div className="input">
+                        <input type="email" placeholder="Email Address" ref={emailRef}/>
 
-            <div className="input">
-                <input type="email" placeholder="Email Address" ref={emailRef}/>
+                        <button className="registerButton" onClick={handleStart}>Get Started</button>
+                    </div>
+                ):(
+                    <form className="input">
+                        <input type="password" placeholder="Password" ref={passwordRef}/>
 
-                <button className="registerButton" onClick={handleStart}>Get Started</button>
-            </div>
+                        <button className="registerButton" onClick={handleFinish}>Start</button>
+                    </form>
+                )
+            }
         </div>
 
 
